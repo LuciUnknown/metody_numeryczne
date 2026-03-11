2f153abd2c1691b0cf909f85914dbd9d2291335f
LuciUnknown <bartlomiejstasiak20@gmail.com>
2026-03-11 20:29:19 +0100

    trygonometria ale idk czy taka jej pasuje

---
+++ trygonometria.py
@@ -0,0 +1,13 @@
+import math
+
+def sin(x):
+    return round(math.sin(math.radians(x)), 3)
+
+def cos(x):
+    return round(math.cos(math.radians(x)), 3)
+
+def tan(x):
+    return round(math.tan(math.radians(x)), 3)
+
+def ctan(x):
+    return round(1/math.tan(math.radians(x)), 3)
