000387a94f47d0b16b81e93dca05d8f11f8a4ee6
Michał Pietrasiak <254844@edu.p.lodz.pl>
2026-03-11 21:33:52 +0100

    Chyba poprawna sieczna, bez wartości bezwględnych

---
+++ sieczna.py
@@ -0,0 +1,56 @@
+import horner
+
+def sieczna_epsilon(a,b, tab, e):
+    if horner.horner(a, tab) == 0.0:
+        return a
+    if horner.horner(b, tab) == 0.0:
+        return b
+
+    while b - a > e:
+        f_a = horner.horner(a, tab)
+        f_b = horner.horner(b, tab)
+
+        if f_a - f_b == 0:
+            return b
+
+        x = b - f_b * (b - a) / (f_b  - f_a)
+
+        if horner.horner(x, tab) == 0.0:
+            return x
+
+        a = b
+        b = x
+
+    return b
+
+
+    # return (a+b)/2
+
+def sieczna_iter(a,b, tab, stop):
+    i = 0
+    if horner.horner(a, tab) == 0.0:
+        return a
+    if horner.horner(b, tab) == 0.0:
+        return b
+
+    while i < stop:
+        f_a = horner.horner(a, tab)
+        f_b = horner.horner(b, tab)
+
+        if f_a - f_b == 0:
+            return b
+
+        x = b - f_b * (b - a) / (f_b - f_a)
+
+        if horner.horner(x, tab) == 0.0:
+            return x
+
+        a = b
+        b = x
+        i+=1
+
+    return b
+
+
+print(sieczna_epsilon(-10, 10, [1,-3,2,-6], 0.00000001))
+print(sieczna_iter(-10, 10, [1,-3,2,-6], 20))
