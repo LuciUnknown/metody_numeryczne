ac7a76be5018738a7328e87896872cfd3d8b0a5f
LuciUnknown <bartlomiejstasiak20@gmail.com>
2026-05-12 14:23:18 +0200

    więcej funkcji

--- zad4/main.py
+++ zad4/main.py
@@ -3,13 +3,27 @@ from legendre import gauss_legendre_quadrature
 from newton_cotes import newton_cotes
 
 
-def funkcja_podcalkowa(x):
-    return np.sin(x) + x**2
+def funkcja_podcalkowa(val):
+    if val == '1':
+        return lambda x: np.sin(x) + x ** 2
+    elif val == '2':
+        return lambda x: pow(x, 3) - 2*pow(x, 1) + 5
+    elif val == '3':
+        return lambda x: np.sqrt(x)
+
+    return None
 
 
 def main():
     while True:
         print("\n=== Menu Całkowania Numerycznego ===")
+        print("Wybierz funkcje")
+        print("1. f(x) = sin(x) + x^2")
+        print("2. f(x) = (x^3 - 2x + 5)")
+        print("3. f(x) = sqrt(x)")
+
+        fun = input("Wybierz numer funkcji (1-2): ")
+
         print("1. Kwadratura Gaussa-Legendre'a (2-5 węzłów)")
         print("2. Metoda Newtona-Cotesa (złożona Simpsona)")
         print("3. Zakończ program")
@@ -27,12 +41,12 @@ def main():
                 if wybor == '1':
                     print(f"\nWyniki dla kwadratury Gaussa-Legendre'a w przedziale [{a}, {b}):")
                     for n in range(2, 6):
-                        wynik = gauss_legendre_quadrature(funkcja_podcalkowa, a, b, n)
+                        wynik = gauss_legendre_quadrature(funkcja_podcalkowa(fun), a, b, n)
                         print(f"Dla n={n} węzłów: {wynik:.8f}")
                 
                 elif wybor == '2':
                     epsilon = float(input("Podaj dokładność (epsilon, np. 0.0001): "))
-                    wynik, iteracje = newton_cotes(funkcja_podcalkowa, a, b, epsilon)
+                    wynik, iteracje = newton_cotes(funkcja_podcalkowa(fun), a, b, epsilon)
                     print(f"\nWynik całkowania: {wynik:.8f}")
                     print(f"Liczba iteracji: {iteracje}")
             
